e4539b6721b69a95393d24ac3c1442c7d8950508
Johannes <zelger.johannes@gmail.com>
2021-06-01 13:21:40 +0100

    added total to vesting

--- src/app/vesting/vesting-table/vesting-table.component.ts
+++ src/app/vesting/vesting-table/vesting-table.component.ts
@@ -28,6 +28,16 @@ export class VestingTableComponent implements OnInit, OnChanges {
     this.cols = Object.assign([], this.stdCols);
     if (this.data !== undefined && this.data[0] !== undefined && this.data[0].dates !== undefined) {
       this.data[0].dates.forEach(d => this.cols.push({field: 'dates', header: d, type: 'date'}));
+
+      const totalDebit = [];
+      const totalCredit = [];
+      for (let i = 0; i < this.data[0].dates.length; i++) {
+        totalDebit.push(this.data.map(data => data.debit[i]).reduce((sum, current) => sum + current, 0));
+        totalCredit.push(this.data.map(data => data.credit[i]).reduce((sum, current) => sum + current, 0));
+      }
+
+      this.data.push({grantName: 'Total', debit: totalDebit, credit: totalCredit, dates: this.data[0].dates});
+      this.data = this.data.splice(0);
     }
   }
 
